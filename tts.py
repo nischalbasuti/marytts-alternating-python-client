4f1bce9ae729d1ed9a2d0a332fa1936637d3344f
nischal basuti <nischalbasuti1@gmail.com>
2017-10-18 15:56:01 +0530

    added voice option

--- tts.py
+++ tts.py
@@ -74,11 +74,25 @@ class TTS(object):
 
     # Converts wav files to mp3 using ffmpeg
     def _wav2mp3(self, prefix, count):
-        cmd = "ffmpeg -i {0}_wav/{1}.wav {0}_mp3/{1}.mp3".format(prefix, count)
+        cmd = "ffmpeg -i {0}_wav/{1}.wav {0}_mp3/{1}.mp3".format(prefix, "%02d"%count)
         subprocess.call(cmd, shell=True)
 
     # todo
-    def _concatinate_mp3(self):
+    def _concatinate_mp3(self, files):
+        concatString = "\"concat:{0}_wav/00.wav".format(self.outputPrefix)
+
+        concatString = "\"concat:%s" % ("|".join(files))
+        concatString += "\""
+        print(concatString)
+        subprocess.call("ffmpeg -i %s -acodec copy %s.mp3" % (concatString, self.outputPrefix), shell = True)
+    # todo
+    def setVoices(self, voice):
+        if ( voice.lower() == "male" ) or ( voice.lower() == "m" ):
+            self.voices[0] = "dfki-spike-hsmm"
+            self.voices[1] = "cmu-slt-hsmm"
+        elif ( voice.lower() == "female" ) or ( voice.lower() == "f" ):
+            self.voices[0] = "cmu-slt-hsmm"
+            self.voices[1] = "dfki-spike-hsmm"
         pass
 
     # The self.run function() coverts text to speech
@@ -88,6 +102,10 @@ class TTS(object):
     def run(self):
         voiceSwitch = True    #boolean, used to alternate voices
         count = 0   #name files based on count
+
+        wav_files = []
+        mp3_files = []
+
         for line in self.lines:
             if voiceSwitch:
                 voice = self.voices[0]
@@ -104,18 +122,22 @@ class TTS(object):
             # Decode the wav file or raise an exception if no wav files
             if (resp["content-type"] == "audio/x-wav"):
                 # Write the wav file
-                f = open("./%s_wav/%s.wav" % (self.outputPrefix, str( count )), "wb")
+                f = open("./%s_wav/%02d.wav" % (self.outputPrefix, count), "wb")
                 f.write(content)
                 f.close()
 
                 # Convert to mp3
                 self._wav2mp3(self.outputPrefix, count)
 
+                wav_files.append("./%s_wav/%02d.wav" % (self.outputPrefix, count))
+                mp3_files.append("./%s_mp3/%02d.mp3" % (self.outputPrefix, count))
+
                 count = count + 1
             else:
                 raise Exception(content)
+        self._concatinate_mp3(mp3_files)
         # Remove wav files and directory
-        if self.toClean == "True":
+        if self.toClean.lower() == "true":
             subprocess.call(['rm', '-r', "%s_wav" % (self.outputPrefix)])
 
 if __name__ == "__main__":
@@ -123,12 +145,17 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument('-i', '--input', required = True, help = 'input text file' )
     ap.add_argument('-o', '--output', required = True, help = 'output dir name and prefix' )
-    ap.add_argument('-c', '--clean', required = False, help = 'set True to clean files. default is False' )
+    ap.add_argument('-v1', '--voice1', required = False, help = 'set gender of first voice1, default is female/f.' )
+    ap.add_argument('-c', '--clean', required = False, help = 'set True to clean wav files. default is False' )
 
     args = vars(ap.parse_args())
     textFilePath = args['input']
     outputPrefix = args['output']
     toClean = args['clean']
+    if not toClean:
+        toClean = 'false'
+    voice1 = args['voice1']
 
     tts = TTS(textFilePath, outputPrefix, toClean)
+    tts.setVoices(voice1)
     tts.run()
